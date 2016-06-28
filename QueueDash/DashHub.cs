4f1e4246c771d1468b834bfb6a8574b5f871024b
Crowther, Tim <tim.crowther@burrows.yr.com>
2016-06-28 15:55:58 +0100

    WIP

---
+++ QueueDash/DashHub.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.SignalR;
+using QueueDash.Models;
+using QueueDash.Presenters;
+
+namespace QueueDash
+{
+    public class DashHub : Hub
+    {
+        private readonly IQueueDashPresenter _presenter;
+
+        public DashHub(IQueueDashPresenter presenter)
+        {
+            _presenter = presenter;
+        }
+
+        public void Hello()
+        {
+            DashboardViewModel vm = _presenter.GetDashboardData();
+            List<QueueData> queues = vm.Queues;
+
+            dynamic caller = Clients.Caller;
+            caller.populateQueues(queues.ToArray());
+        }
+    }
+}
