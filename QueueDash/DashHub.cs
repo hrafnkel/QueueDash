02df7b44475c37704911ecd95d3955c1107e3bd6
hrafnkel <hrafnkel@test.com>
2016-08-04 14:22:38 +0100

    Signal only sent if the data has changed.

--- QueueDash/DashHub.cs
+++ QueueDash/DashHub.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
 using QueueDash.Models;
@@ -11,6 +13,7 @@ namespace QueueDash
     public class DashHub : Hub
     {
         readonly QueueRepository _repository = new QueueRepository();
+        private List<QueueData> _oldQueueData = new List<QueueData>();
 
         public void Hello()
         {
@@ -20,15 +23,35 @@ namespace QueueDash
 
         public void Refresh(bool calledFromTest)
         {
-            int timeout = 50000;
+            double timeout = TimeSpan.FromMilliseconds(50000).TotalMilliseconds;
             Stopwatch sw = Stopwatch.StartNew();
             while (true)
             {
                 List<QueueData> queues = _repository.GetDashboardData();
-                Clients.All.populateQueues(queues);
-                System.Threading.Thread.Sleep(1000);
+                if (QueueDataHasChanged(queues)) Clients.All.populateQueues(queues);
+                System.Threading.Thread.Sleep(2000);
                 if (calledFromTest && (sw.ElapsedMilliseconds > timeout)) break;
             }
+        }
+
+        private bool QueueDataHasChanged(List<QueueData> queues)
+        {
+            if (_oldQueueData.Count != queues.Count)
+            {
+                _oldQueueData = queues;
+                return true;
+            }
+
+            if ((from data in queues
+                 let queueData = _oldQueueData.FirstOrDefault(x => x.Name == data.Name)
+                 where (queueData == null)||(queueData.Name != data.Name)||(queueData.Depth != data.Depth)
+                 select data).Any())
+            {
+                _oldQueueData = queues;
+                return true;
+            }
+
+            return false;
         }
     }
 }
